4784e6d533cf3aef26a306a03d606033e64c809f
Shakeel Mohamed <contact@shakeelmohamed.com>
2015-01-26 21:22:27 -0800

    Search by geolocation gets hotels from firebase via geofire
    * Search by string is not implemented yet
    * Thumbnails are currently mocked up with the ionic logo

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,7 @@
  * 8. Wait...
  */
 
-var exbedia = angular.module('exbedia', ['ionic', 'ngCordova']);
+var exbedia = angular.module('exbedia', ['ionic', 'ngCordova', 'firebase', 'angularGeoFire']);
 
 exbedia.config(function($locationProvider) {
   $locationProvider.html5Mode(true);
@@ -43,14 +43,53 @@ exbedia.controller('SeachController', function($scope, $cordovaGeolocation) {
 /**
  * Controllers for results view
  */
-exbedia.controller('SearchParamsController', function($scope, $location) {
+
+function firebaseAuth(error) {
+  if (error) {
+    alert('Failed to authenticate to Firebase using token:'+ AUTH);
+  }
+}
+exbedia.controller('SearchParamsController', function($scope, $location, $firebase, $geofire) {
+  // Get the querystring parameters; temporarily being displayed as raw JSON
+  $scope.params = $location.search();
+
+  // Uses JS to go back 1 page in the browser history, which should be the search page
   $scope.gotoSearch = function() {
     history.back();
   };
-  var queryObject = $location.search();
-  $scope.params = queryObject;
+
+  $scope.hotels = [];
+
+  // Below is all the code required to do a search for hotels based on geolocation
+  var AUTH = 'UPDATE ME THIS IS NOT A REAL KEY';
+  var hotels_url = 'https://glowing-heat-3430.firebaseio.com/hotels';
+  var geodata_url = 'https://glowing-heat-3430.firebaseio.com/geohotels';
+  var fb_hotels = new Firebase(hotels_url);
+  fb_hotels.authWithCustomToken(AUTH, firebaseAuth);
+  var fb_geodata = new Firebase(geodata_url);
+  fb_geodata.authWithCustomToken(AUTH, firebaseAuth);
+
+  var geoFire = $geofire(fb_geodata);
+    
+  var geoFireQuery = geoFire.$query({
+      center: [parseFloat($scope.params.lat), parseFloat($scope.params.lon)],
+      radius: 10
+  });
+
+  // Setup Angular Broadcast event for when an object enters our query
+  var geoQueryCallback = geoFireQuery.on("key_entered", "SEARCH:KEY_ENTERED");
   
-  $scope.results = function(num) {
-    return new Array(num);
-  };
+  var numResults = 20; // TODO: temporarily hardcoded
+  var i = 0;
+  // Listen for Angular Broadcast
+  $scope.$on("SEARCH:KEY_ENTERED", function (event, hotelID, location, distance) {
+    if (i++ >= 20) return; // Stop at 20
+    var hotelResult = $firebase(fb_hotels.child(hotelID));
+    var hotelObject = hotelResult.$asObject();
+    console.log(hotelObject);
+    if (hotelObject) {
+      // TODO: figure out how to get the distance on the hotelObjeect; just setting a property doesn't work
+      $scope.hotels.push(hotelObject);
+    }
+  });
 });
