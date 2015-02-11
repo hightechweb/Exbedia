55876915cced597f9bb10849c28cfd4fdfd3b634
biancaflaidar <flaidarb@seattleu.edu>
2015-02-10 17:36:45 -0800

    search template
    current location works

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -2,17 +2,21 @@
  * Controllers for search view
  */
 exbedia.controller('SearchController', function($scope, $cordovaGeolocation) {
-  // $scope.getLoc = function() {
-    // if ($scope.useCurrentLocation) {
-      // navigator.geolocation.getCurrentPosition(function(position){
-        // var lat  = position.coords.latitude;
-        // var lon = position.coords.longitude;
-        // $scope.$apply(function() {
-          // $scope.lat = lat;
-          // $scope.lon = lon;
-        // });
-      // });
-    // }
-  // };
-  console.log("search page");
+    $scope.query = {};
+  $scope.getLoc = function() {
+    if ($scope.useCurrentLocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            $scope.$apply(function() {
+              $scope.query.lat = lat;
+              $scope.query.lon = lon;
+            });
+        });
+    }
+  };
+  
+  $scope.formSubmit = function(query) {
+    console.log(query);
+  };
 });
