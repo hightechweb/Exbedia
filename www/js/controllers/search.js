1075889dc96744c2f36d1b4997c690f47e69aed3
Kim Nguyen <nguyen91@seattleu.edu>
2015-03-06 18:11:54 -0800

    Autocomplete-ion

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -1,5 +1,11 @@
 exbedia.controller('SearchController', function($scope, $location, $cordovaGeolocation) {
     $scope.query = {};
+    $scope.googlePlaceData = null;
+    //restricting search option to US only
+    $scope.autocompleteOptions = {
+        componentRestrictions: { country: 'us' },
+        types: ['geocode']
+    };
     
     $scope.getLoc = function() {
         if ($scope.useCurrentLocation) {
@@ -12,7 +18,28 @@ exbedia.controller('SearchController', function($scope, $location, $cordovaGeolo
         }
     };
   
-    $scope.formSubmit = function(query) {
+    $scope.formSubmit = function() {
+        // Default to current location
+        var query = $scope.query;
+
+        // Overwrite those values if using a Google Place
+        if (!$scope.useCurrentLocation &&
+            $scope.googlePlaceData &&
+            $scope.googlePlaceData.geometry &&
+            $scope.googlePlaceData.geometry.location &&
+            $scope.googlePlaceData.geometry.location.hasOwnProperty("k") &&
+            $scope.googlePlaceData.geometry.location.hasOwnProperty("D")
+            ) {
+            query.lat = $scope.googlePlaceData.geometry.location.k;
+            query.lon = $scope.googlePlaceData.geometry.location.D;
+        }
+
+        // Abort if bad values.
+        if (!query || !query.hasOwnProperty("lat") || !query.hasOwnProperty("lon")) {
+            alert("Please pick a location");
+            return;
+        }
+
         console.log("We are trying to submit now...");
         // Navigate to the results view with the specified parameters
         $location.search(query);
