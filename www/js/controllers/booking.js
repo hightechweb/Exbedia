ed98122c700501132529ccd83033f85dcaa63801
Kim Nguyen <nguyen91@seattleu.edu>
2015-04-14 14:56:53 -0700

    Booking controller

---
+++ www/js/controllers/booking.js
@@ -0,0 +1,15 @@
+exbedia.controller('BookingController', function($rootScope, $location){
+
+	// Take user to confirmation page
+	$rootScope.navigateToConfirm = function(hotelObject){
+		if (hotelObject && hotelObject.hasOwnProperty("id")){
+			$rootScope.hotel = hotelObject;
+			$location.path("/confirmation:" + hotelObject.id);
+		}
+		else {
+			// TODO: handle error
+			console.log("ERROR, hotelID was not defined. Cannot go anywhere.");
+			return;
+		}
+	};
+});
