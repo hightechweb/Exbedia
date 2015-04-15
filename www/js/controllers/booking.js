6db70a1f1682d35743dffb98920b99a26e5e043f
Kim Nguyen <nguyen91@seattleu.edu>
2015-04-15 11:32:13 -0700

    little changes in booking

--- www/js/controllers/booking.js
+++ www/js/controllers/booking.js
@@ -1,8 +1,8 @@
 exbedia.controller('BookingController', function($rootScope, $location){
 
 	// Take user to confirmation page
-	$rootScope.navigateToConfirm = function(hotelObject){
-		if (hotelObject && hotelObject.hasOwnProperty("id")){
+	$rootScope.navigateToConfirm = function(hotelObject) {
+		if (hotelObject && hotelObject.hasOwnProperty("id")) {
 			$rootScope.hotel = hotelObject;
 			$location.path("/confirmation:" + hotelObject.id);
 		}
