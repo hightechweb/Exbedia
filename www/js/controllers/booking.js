293bec094ab11a43f1d40773fb135b19f907300e
Shakeel Mohamed <smohamed@splunk.com>
2015-04-21 23:37:49 -0700

    Make back button work booking -> details

--- www/js/controllers/booking.js
+++ www/js/controllers/booking.js
@@ -1,17 +1,21 @@
 exbedia.controller('BookingController', function($rootScope, $location){
+    $rootScope.bookingInfo = $rootScope.bookingInfo || {};
 
-	// Take user to confirmation page
-	$rootScope.navigateToConfirm = function(hotelObject) {
-		if (hotelObject && hotelObject.hasOwnProperty("id")) {
-			$rootScope.hotel = hotelObject;
-			$location.path("/confirmation:" + hotelObject.id);
-		}
-		else {
-			// TODO: handle error
-			console.log("ERROR, hotelID was not defined. Cannot go anywhere.");
-			return;
-		}
-	};
+    // Take user to confirmation page
+    $rootScope.navigateToConfirm = function(hotelObject) {
+        if (hotelObject && hotelObject.hasOwnProperty("id")) {
+            $rootScope.hotel = hotelObject;
+            $location.path("/confirmation:" + hotelObject.id);
+        }
+        else {
+            // TODO: handle error
+            console.log("ERROR, hotelID was not defined. Cannot go anywhere.");
+            return;
+        }
+    };
 
-	$rootScope.bookingInfo = {};
+    $rootScope.navigateToDetails = function() {
+        // navigate to details view for the previously viewed hotel
+        $location.path('/details:' + $rootScope.hotel.id);
+    };
 });
