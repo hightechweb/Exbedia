5eff31fbda92ed4451c105806d4ead25bc0f1e04
Kevin Luo <kevinluo@live.com>
2015-05-11 21:56:23 -0700

    Store booking data in Firebase

--- www/js/controllers/booking.js
+++ www/js/controllers/booking.js
@@ -1,11 +1,28 @@
-exbedia.controller('BookingController', function($rootScope, $location){
+exbedia.controller('BookingController', function($rootScope, $location, $firebase){
     $rootScope.bookingInfo = $rootScope.bookingInfo || {};
 
     // Take user to confirmation page
-    $rootScope.navigateToConfirm = function(hotelObject) {
+    $rootScope.navigateToConfirm = function(hotelObject, bookingInfo) {
         if (hotelObject && hotelObject.hasOwnProperty("id")) {
             $rootScope.hotel = hotelObject;
-            $location.path("/confirmation:" + hotelObject.id);
+
+            var bookingKeys = [
+                "checkInDate",
+                "checkOutDate",
+                "num_guests",
+                "FName",
+                "LName",
+                "email",
+                "tel"
+            ];
+            for (var k in bookingKeys) {
+                if (!bookingInfo.hasOwnProperty(bookingKeys[k]) || !bookingInfo[bookingKeys[k]]) {
+                    // missing data
+                    console.log("Missing the ", bookingKeys[k], " field.");
+                    return;
+                    }
+            }
+            addBookingToFirebase(hotelObject, bookingInfo);
         }
         else {
             // TODO: handle error
@@ -23,4 +40,34 @@ exbedia.controller('BookingController', function($rootScope, $location){
         // See how it was done in `navigateToBooking` in details.js
         $location.path('/details:' + $rootScope.hotel.id);
     };
-});
+
+    function generateRandomNum(hotelName) {
+        var currentDdate = (new Date()).valueOf().toString();
+        var str = hotelName + currentDdate;
+        var hash = 0, strlen = str.length, i, c;
+        if(strlen == 0) return hash;
+        for( i = 0; i < strlen; i++) {
+            c = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + c;
+            hash = hash & hash;
+        }
+        return (hash >>> 0);
+    };
+
+    var firebaseUrl = 'https://test-exbedia.firebaseio.com';
+    var firebaseBookings = new Firebase(firebaseUrl + '/bookings');
+
+    function addBookingToFirebase(hotelObject, bookingInfo) {
+        $rootScope.bookingID = generateRandomNum(hotelObject.Name);
+        var booking = {
+            checkInDate: bookingInfo.checkInDate,
+            checkOutDate: bookingInfo.checkOutDate,
+            firstName: bookingInfo.FName,
+            lastName: bookingInfo.LName,
+            tel: bookingInfo.tel,
+        };
+        firebaseBookings.child($rootScope.bookingID).set(booking);
+
+        $location.path("/confirmation:" + hotelObject.id);
+    } 
+})
